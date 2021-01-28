076d45647965bf34b0786fb0ec5ef607a0a1d422
bjang <peterbjang@gmail.com>
2021-01-27 19:38:16 -0500

    Working version 1

---
+++ search.py
@@ -0,0 +1,67 @@
+#Continuing Education - Search Algorithms
+#Exercises written by Cyber Pete
+
+#Exercise 1
+def LinearSearch(list, element):
+    """
+    input: (list, int)
+    list: list containing elements to search
+    element: integer being searched for
+    
+    returns: (list, int)
+    If the element does not exist in the list return the searched list and None (list, None)
+    list: the list of elements that have been searched
+    int: the index at which the element was found
+    """
+    #TODO
+    #Write your code here
+    pass
+
+#Exercise 2
+def BinarySearch(list, element):
+    """
+    input: (list, int)
+    list: sorted list containing elements to search
+    element: integer being searched for
+
+    returns: tuple - (list, int)
+    If the element does not exist in the list return the searched list and None (list, None)
+    list: the list of elements that have been searched
+    int: the index at which the element was found
+    """
+    #TODO
+    #Write your code here
+    pass
+
+#Exercise 3
+def BreadthFirstSearch(graph, start, goal):
+    """
+    input: (dictionary, string, string)
+    graph: dictionary containing the graph structure
+    start: string representing the node to start the search
+    goal: string representing the node to find
+
+    returns: tuple - (list(list), list)
+    If the goal node does not exist in the graph return the searched list and None (list(list), None)
+    list(list): the list of lists that contain the paths the algorithm has searched through
+    list: the actual path the algorithm took to find the goal node
+    """
+    #TODO
+    #Write your code here
+    pass
+
+#Exercise 4
+def DepthFirstSearch(graph, start, goal):
+    """
+    input: (dictionary, string, string)
+    graph: dictionary containing the graph structure
+    start: string representing the node to start the search
+    goal: string representing the node to find
+
+    returns: list
+    If the element does not exist in the graph return None
+    list: the actual path the algorithm took to find the goal node
+    """
+    #TODO
+    #Write your code here
+    pass
---
+++ test.py
@@ -0,0 +1,106 @@
+#Continuing Education - Search Algorithms
+#Tests written by Cyber Pete
+
+import unittest
+from search import LinearSearch, BinarySearch, BreadthFirstSearch, DepthFirstSearch
+
+class TestLinearSearch(unittest.TestCase):
+    def test_simple1(self):
+        short_list = [5,3,1,2,7,6,9,8]
+        self.assertEqual(LinearSearch(short_list, 6), ([5, 3, 1, 2, 7, 6], 5))
+    def test_simple2(self):
+        short_list = [1,7,9,4,5,6,2,3,8]
+        self.assertEqual(LinearSearch(short_list, 1), ([1], 0))
+    def test_simple3(self):
+        short_list = [1,2,3,4,5,6,7,8,9]
+        self.assertEqual(LinearSearch(short_list, 13), ([1,2,3,4,5,6,7,8,9], None))
+
+class TestBinarySearch(unittest.TestCase):
+    def test_binary1(self):
+        binary_list = [1,2,3,4,5,6,7]
+        self.assertEqual(BinarySearch(binary_list, 3), ([4, 2, 3], 2))
+    def test_binary2(self):
+        binary_list = [1,2,3,4,5,6,7]
+        self.assertEqual(BinarySearch(binary_list, 5), ([4, 6, 5], 4))
+    def test_binary3(self):
+        binary_list = [2,6,12,15,18,23,42]
+        self.assertEqual(BinarySearch(binary_list, 19), ([15, 23, 18], None))
+
+class TestBreadthFirstSearch(unittest.TestCase):
+    def test_breadth1(self):
+        graph = {
+            'A': ['B', 'C'],
+            'B': ['D', 'E'],
+            'C': ['F', 'G'],
+            'D': ['H', 'I'],
+            'E': ['J', 'K'],
+            'F': ['L', 'M'],
+            'G': ['N', 'O'],
+        }
+        self.assertEqual(BreadthFirstSearch(graph, 'A', 'M'), ([['A', 'B'], ['A', 'C'], ['A', 'B', 'D'], ['A', 'B', 'E'], 
+        ['A', 'C', 'F'], ['A', 'C', 'G'], ['A', 'B', 'D', 'H'], ['A', 'B', 'D', 'I'], ['A', 'B', 'E', 'J'], ['A', 'B', 'E', 'K'], 
+        ['A', 'C', 'F', 'L'], ['A', 'C', 'F', 'M']], ['A', 'C', 'F', 'M']))
+    def test_breadth2(self):
+        graph = {
+            'A': ['B', 'C'],
+            'B': ['D', 'E'],
+            'C': ['F', 'G'],
+            'D': ['H', 'I'],
+            'E': ['J', 'K'],
+            'F': ['L', 'M'],
+            'G': ['N', 'O'],
+        }
+        self.assertEqual(BreadthFirstSearch(graph, 'A', 'E'), ([['A', 'B'], ['A', 'C'], ['A', 'B', 'D'], ['A', 'B', 'E']], ['A', 'B', 'E']))
+    def test_breadth3(self):
+        graph = {
+            'A': ['B', 'C'],
+            'B': ['D', 'E'],
+            'C': ['F', 'G'],
+            'D': ['H', 'I'],
+            'E': ['J', 'K'],
+            'F': ['L', 'M'],
+            'G': ['N', 'O'],
+        }
+        self.assertEqual(BreadthFirstSearch(graph, 'A', 'P'), ([['A', 'B'], ['A', 'C'], ['A', 'B', 'D'], ['A', 'B', 'E'], ['A', 'C', 'F'], 
+        ['A', 'C', 'G'], ['A', 'B', 'D', 'H'], ['A', 'B', 'D', 'I'], ['A', 'B', 'E', 'J'], ['A', 'B', 'E', 'K'], ['A', 'C', 'F', 'L'], 
+        ['A', 'C', 'F', 'M'], ['A', 'C', 'G', 'N'], ['A', 'C', 'G', 'O']], None))
+
+class TestDepthFirstSearch(unittest.TestCase):
+    def test_depth1(self):
+        graph = {
+            'A': ['B', 'C'],
+            'B': ['D', 'E'],
+            'C': ['F', 'G'],
+            'D': ['H', 'I'],
+            'E': ['J', 'K'],
+            'F': ['L', 'M'],
+            'G': ['N', 'O'],
+        }
+        self.assertEqual(DepthFirstSearch(graph, 'A', 'K'), ['A', 'B', 'E', 'K'])
+    
+    def test_depth2(self):
+        graph = {
+            'A': ['B', 'C'],
+            'B': ['D', 'E'],
+            'C': ['F', 'G'],
+            'D': ['H', 'I'],
+            'E': ['J', 'K'],
+            'F': ['L', 'M'],
+            'G': ['N', 'O'],
+        }
+        self.assertEqual(DepthFirstSearch(graph, 'A', 'M'), ['A', 'C', 'F', 'M'])
+
+    def test_depth3(self):
+        graph = {
+            'A': ['B', 'C'],
+            'B': ['D', 'E'],
+            'C': ['F', 'G'],
+            'D': ['H', 'I'],
+            'E': ['J', 'K'],
+            'F': ['L', 'M'],
+            'G': ['N', 'O'],
+        }
+        self.assertEqual(DepthFirstSearch(graph, 'A', 'Q'), None)
+
+if __name__ == '__main__':
+    unittest.main()
